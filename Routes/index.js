803b360cf639073630d6f858a1cea7181e98ff99
soniya <soniyamaldev@gmail.com>
2018-03-14 14:08:42 +0530

    guiasdq3wASDFE

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
-const createHandler = require('github-webhook-handler');
-const handler = createHandler({ path: '/webhook', secret: 'webhook' });
+/*const createHandler = require('github-webhook-handler');
+const handler = createHandler({ path: '/webhook', secret: 'webhook' });*/
 
 router.get('/test',(req,res)=>{
     console.log('test');
@@ -10,26 +10,26 @@ router.get('/test',(req,res)=>{
 
 
 router.post('/webhook',(req,res)=>{
-    //console.log(req.body);
+    console.log(req.body);
     res.sendStatus(200);
 });
 router.get('/',(req,res)=>{
     res.send('this is a test webhook server!');
 });
 
-handler.on('push', function (event) {
+/*handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
         event.payload.ref)
 });
 handler.on('issues', function (event) {
-    /*console.log('Received an issue event for %s action=%s: #%d %s',
+    /!*console.log('Received an issue event for %s action=%s: #%d %s',
         event.payload.repository.name,
         event.payload.action,
         event.payload.issue.number,
-        event.payload.issue.title)*/
+        event.payload.issue.title)*!/
     console.log('issues');
-});
+});*/
 
 /*router.get('/webhook',(req,res)=>{
     res.send()
