277bd8ab14b08dbefa4ba68e560eab0390ceb911
soniya <soniyamaldev@gmail.com>
2018-03-14 13:00:35 +0530

    webhook handler issues

--- Routes/index.js
+++ Routes/index.js
@@ -16,6 +16,13 @@ handler.on('push', function (event) {
         event.payload.repository.name,
         event.payload.ref)
 });
+handler.on('issues', function (event) {
+    console.log('Received an issue event for %s action=%s: #%d %s',
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title)
+});
 
 /*router.get('/webhook',(req,res)=>{
     res.send()
