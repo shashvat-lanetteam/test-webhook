b8eaaba875fa5ac0071f28884f4f01f0893704ca
soniya <soniyamaldev@gmail.com>
2018-03-14 12:58:24 +0530

    webhook handler

--- Routes/index.js
+++ Routes/index.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const router=express.Router();
+const createHandler = require('github-webhook-handler');
+const handler = createHandler({ path: '/webhook', secret: 'webhooksecret' });
 
 router.get('/test',(req,res)=>{
    res.send('test1');
@@ -9,4 +11,13 @@ router.get('/',(req,res)=>{
     res.send('this is a test webhook server!');
 });
 
+handler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref)
+});
+
+/*router.get('/webhook',(req,res)=>{
+    res.send()
+});*/
 module.exports={router};
