66316bab22e07a79a63fbec7114b714fbca97edf
soniya <soniyamaldev@gmail.com>
2018-03-14 15:17:26 +0530

    guiasdq3wASDFE

--- Routes/index.js
+++ Routes/index.js
@@ -1,45 +1,10 @@
 const express=require('express');
 const router=express.Router();
-const createHandler = require('github-webhook-handler');
-const handler = createHandler({ path: '/webhook', secret: 'webhook' });
+
 
 router.get('/test',(req,res)=>{
     console.log('test');
    res.send('test1');
-});
-
-
-
-router.post('/webhook',handler);
-router.get('/',(req,res)=>{
-    res.send('this is a test webhook server!');
-});
-
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref)
-    console.log('===========>','push event');
-});
-handler.on('issues', (event)=> {
-    /*console.log('Received an issue event for %s action=%s: #%d %s',
-        event.payload.repository.name,
-        event.payload.action,
-        event.payload.issue.number,
-        event.payload.issue.title)*/
-    console.log('issues');
-});
-handler.on('issue', (event)=> {
-    /*console.log('Received an issue event for %s action=%s: #%d %s',
-        event.payload.repository.name,
-        event.payload.action,
-        event.payload.issue.number,
-        event.payload.issue.title)*/
-    console.log('issue');
-});
-
-handler.on('opened',(event)=>{
-   console.log('opened event');
 });
 
 module.exports={router};
--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {router}=require('./Routes');
+
+const createHandler = require('github-webhook-handler');
+const handler = createHandler({ path: '/webhook', secret: 'webhook' });
+
+
 const Port=process.env.PORT || 1212;
 let app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use('/',router);
+
+app.use('/',handler);
+
+
+handler.on('error', function (err) {
+    console.error('Error:', err.message);
+});
+
+handler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref);
+});
+
+handler.on('opened', function (event) {
+    console.log('opened webhook!!!!!');
+});
+
+handler.on('issues', function (event) {
+    console.log('Received an issue event for %s action=%s: #%d %s',
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title);
+});
 app.listen(Port,()=>{
    console.log(`server running on ${Port}`);
 });
